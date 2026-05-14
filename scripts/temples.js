f14c3e99366356d9d90bf322d02f2c80bbe3224d
Michael Wiseman <mcwiseman97@gmail.com>
2026-05-14 13:20:23 -0500

    Refactor header layout and navigation: enhance styling for header and menu button, adjust flex properties for better alignment

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,7 @@
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 
-hamButton.addEventListener("_______", () => {
-	navigation._______.toggle("_______");
-	hamButton.classList.toggle("open");
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
 });
