570b042d2544a69ef7e716ff2f7d1f301f0c3527
Michael Wiseman <mcwiseman97@gmail.com>
2026-05-19 11:46:34 -0500

    Add last modified date display to place webpage

---
+++ scripts/place.js
@@ -0,0 +1,4 @@
+// Next 3 lines determines the last modified date 
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
