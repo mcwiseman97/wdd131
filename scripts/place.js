a32faaadf265873eff45cd3706bf7c5ae1ea4a51
Michael Wiseman <mcwiseman97@gmail.com>
2026-05-22 15:27:03 -0500

    Fix variable name in wind chill calculation function

--- scripts/place.js
+++ scripts/place.js
@@ -16,6 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-function calculateWindChill(t, v) {
-    return 13.12 + (0.6215 * t) - (11.37 * Math.pow(v, 0.16)) + (0.3965 * t * Math.pow(v, 0.16));
+function calculateWindChill(t, w) {
+    return 13.12 + (0.6215 * t) - (11.37 * Math.pow(w, 0.16)) + (0.3965 * t * Math.pow(v, 0.16));
 }
